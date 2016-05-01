094252ff57a09326020125379b6b67bda164e87b
jingbo.wang <norain2050@qq.com>
2016-05-01 19:38:44 +0800

    use a Node Module:fortune.js

--- meadowlark.js
+++ meadowlark.js
@@ -1,4 +1,5 @@
 var express=require('express');
+var fortune = require('./lib/fortune.js');// import the new Node Module:fortune.js
 
 var app=express();
 
@@ -10,7 +11,7 @@ app.set('view engine','handlebars');
 
 app.set('port',process.env.PORT || 3000);
 
-
+/** Already used Node Modules : ./lib/fortune.js
 var fortunes = [
     "one",
     "two",
@@ -18,37 +19,37 @@ var fortunes = [
     "four",
     "five"
 ];
+**/
+
+
 
 app.use(express.static(__dirname+'/public'));//设置默认的静态文件目录，此目录不会出现在path路径里，但子目录会。必须放在其他访问处理方法的前面。
 
 app.get('/',function(req,res){
-   // res.type('text/plain');
-    //res.send('Meadowlark Travel');
+
     res.render('home');
 });
 
 app.get('/about',function(req,res){
-   // res.type('text/plain');
-   // res.send('About Meadowlark Travel');
-    var randomFortune=fortunes[Math.floor(Math.random()*fortunes.length)];
-    res.render('about',{fortune:randomFortune});
+
+    res.render('about',{fortune:fortune.getFortune()});// use the new Node Module:fortune.js
 });
 
 
 //定制404页面
 app.use(function  (req,res,next) {
-	//res.type('text/plain');
+
 	res.status(404);
-	//res.send('404 - Not Found');
+
     res.render('404');
 });
 
 //定制500页面
 app.use(function(err,req,res,next){
 	console.error(err.stack);
-	//res.type('text/plain');
+
 	res.status(500);
-	//res.send('500 -Server error');
+
     res.render('500');
 });
 
